2aea7ca6fef1548b7bc645105c41d7eb6be646d1
SongshGeo <songshgeo@gmail.com>
2021-02-06 21:00:06 +0800

    successfully imigrated from matlab

---
+++ economic_complexity/genepy_index.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 3.83
+# -*-coding:utf-8 -*-
+# @Author  : Shuang Song
+# @Contact   : SongshGeo@gmail.com
+# GitHub   : https://github.com/SongshGeo
+# Research Gate: https://www.researchgate.net/profile/Song_Shuang9
+
